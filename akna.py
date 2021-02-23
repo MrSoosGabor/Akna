b17c033507933deff4198030ae80a1efc007841d
Soós Gábor <soos.gabor@jedlik.eu>
2021-02-23 09:39:53 +0100

    Az aknák kirajzoltatása

--- akna.py
+++ akna.py
@@ -1,10 +1,34 @@
 import turtle
-import time
+
+
+def szinez(x, y):
+    for s in matrix:
+        for o in s:
+            if o.distance(x, y) < 10:
+                o.color("green")
+
 
 screen = turtle.Screen()
 screen.setup(width=800, height=600)
 screen.bgpic("closing.png")
 
+matrix = []
+screen.tracer(0)
+for sor in range(-100, 100, 20):
+    sorom = []
+    for oszlop in range(-100, 100, 20):
+        hely = turtle.Turtle()
+        hely.shape("circle")
+        hely.color("red")
+        hely.penup()
+        hely.goto(oszlop, sor)
+        sorom.append(hely)
+    matrix.append(sorom)
+
+
+screen.listen()
+screen.onclick(szinez)
+
 while True:
+
     screen.update()
-    time.sleep(0.3)
